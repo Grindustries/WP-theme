dd8b19e0f4a56e3dd19d8fa7ee2198b98eb59f8f
Grindustries <atomov303@gmail.com>
2017-03-08 14:26:12 +1300

    Add files via upload

--- smallbusiness/functions.php
+++ smallbusiness/functions.php
@@ -37,28 +37,38 @@ function create_post_type() {
     );
 }
 
+add_action('init', 'create_customer');
+
+function create_customer() {
+    register_post_type('testimonials', array(
+        'labels' => array(
+            'name' => __('Testimonials'),
+            'singular_name' => __('Testimonials')
+        ),
+        'public' => true,
+        'has_archive' => true,
+            )
+    );
+}
+
 // hook into the init action and call create_book_taxonomies when it fires
-
-
-
-
-//add_action('init', 'create_book_taxonomies', 0);
+add_action('init', 'create_testim_taxonomies', 0);
 
 // create two taxonomies, genres and writers for the post type "book"
-function create_book_taxonomies() {
+function create_testim_taxonomies() {
     // Add new taxonomy, make it hierarchical (like categories)
     $labels = array(
-        'name' => _x('Genres', 'taxonomy general name', 'textdomain'),
-        'singular_name' => _x('Genre', 'taxonomy singular name', 'textdomain'),
-        'search_items' => __('Search Genres', 'textdomain'),
-        'all_items' => __('All Genres', 'textdomain'),
-        'parent_item' => __('Parent Genre', 'textdomain'),
-        'parent_item_colon' => __('Parent Genre:', 'textdomain'),
-        'edit_item' => __('Edit Genre', 'textdomain'),
-        'update_item' => __('Update Genre', 'textdomain'),
-        'add_new_item' => __('Add New Genre', 'textdomain'),
-        'new_item_name' => __('New Genre Name', 'textdomain'),
-        'menu_name' => __('Genre', 'textdomain'),
+        'name' => _x('Customer Types', 'taxonomy general name', 'textdomain'),
+        'singular_name' => _x('Customer Type', 'taxonomy singular name', 'textdomain'),
+        'search_items' => __('Search Customer Types', 'textdomain'),
+        'all_items' => __('All Customer Types', 'textdomain'),
+        'parent_item' => __('Parent Customer Type', 'textdomain'),
+        'parent_item_colon' => __('Parent Customer Type:', 'textdomain'),
+        'edit_item' => __('Edit Customer Type', 'textdomain'),
+        'update_item' => __('Update Customer Type', 'textdomain'),
+        'add_new_item' => __('Add New Customer Type', 'textdomain'),
+        'new_item_name' => __('New Customer Type Name', 'textdomain'),
+        'menu_name' => __('Customer Type', 'textdomain'),
     );
 
     $args = array(
@@ -67,53 +77,10 @@ function create_book_taxonomies() {
         'show_ui' => true,
         'show_admin_column' => true,
         'query_var' => true,
-        'rewrite' => array('slug' => 'genre'),
+        'rewrite' => array('slug' => 'customer_type'),
     );
 
-    register_taxonomy('genre', array('book'), $args);
+    register_taxonomy('customer_type', array('testimonials'), $args);
 
-    // Add new taxonomy, NOT hierarchical (like tags)
-    $labels = array(
-        'name' => _x('Writers', 'taxonomy general name', 'textdomain'),
-        'singular_name' => _x('Writer', 'taxonomy singular name', 'textdomain'),
-        'search_items' => __('Search Writers', 'textdomain'),
-        'popular_items' => __('Popular Writers', 'textdomain'),
-        'all_items' => __('All Writers', 'textdomain'),
-        'parent_item' => null,
-        'parent_item_colon' => null,
-        'edit_item' => __('Edit Writer', 'textdomain'),
-        'update_item' => __('Update Writer', 'textdomain'),
-        'add_new_item' => __('Add New Writer', 'textdomain'),
-        'new_item_name' => __('New Writer Name', 'textdomain'),
-        'separate_items_with_commas' => __('Separate writers with commas', 'textdomain'),
-        'add_or_remove_items' => __('Add or remove writers', 'textdomain'),
-        'choose_from_most_used' => __('Choose from the most used writers', 'textdomain'),
-        'not_found' => __('No writers found.', 'textdomain'),
-        'menu_name' => __('Writers', 'textdomain'),
-    );
 
-    $args = array(
-        'hierarchical' => false,
-        'labels' => $labels,
-        'show_ui' => true,
-        'show_admin_column' => true,
-        'update_count_callback' => '_update_post_term_count',
-        'query_var' => true,
-        'rewrite' => array('slug' => 'writer'),
-    );
-
-    register_taxonomy('writer', 'book', $args);
 }
-//add_action( 'init', 'create_book_tax' );
-//
-//function create_book_tax() {
-//	register_taxonomy(
-//		'genre',
-//		'book',
-//		array(
-//			'label' => __( 'Genre' ),
-//			'rewrite' => array( 'slug' => 'genre' ),
-//			'hierarchical' => true,
-//		)
-//	);
-//}
--- smallbusiness/index.php
+++ smallbusiness/index.php
@@ -8,15 +8,37 @@ get_header();
 ?>
 <h1>Blog</h1>
 
+
 <?php
-if (have_posts()) : while (have_posts()) : the_post();
-?><h2><?php the_title(); ?></h2>
-<p><?php the_content(); ?></p>
-        <?php
-    endwhile;
-else:
-    ?>
-    <p>No content found.</p>
-<?php
-endif;
+$custom_terms = get_terms('customer_type');
+
+foreach ($custom_terms as $custom_term) {
+    wp_reset_query();
+    $args = array('post_type' => 'testimonials',
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'customer_type',
+                'field' => 'slug',
+                'terms' => $custom_term->slug,
+            ),
+        ),
+    );
+
+    $loop = new WP_Query($args);
+    if ($loop->have_posts()) {
+        echo '<h2>' . $custom_term->name . '</h2>';
+
+        while ($loop->have_posts()) : $loop->the_post();
+            ?><div style="width: 350px; background-color: #398f14;">
+                <h3><?php echo get_the_title(); ?></h3>
+
+                <br>
+
+                <p><?php echo get_the_content(); ?></p>
+            </div><?php
+        endwhile;
+    }
+}
+
+
 get_footer();
